a1d9c820955df91e3998e478eb7905b7e9407d43
HARISH PATIL <134260275+HarishPatil20@users.noreply.github.com>
2026-02-25 20:39:48 +0530

    i changed work details of our web site
    i changes details of works details in prodects

--- src/components/works.tsx
+++ src/components/works.tsx
@@ -105,11 +105,8 @@ export const Works = () => {
             variants={fadeIn("", "", 0.1, 1)}
             className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
           >
-            Following projects showcases my skills and experience through
-            real-world examples of my work. Each project is briefly described
-            with links to code repositories and live demos in it. It reflects my
-            ability to solve complex problems, work with different technologies,
-            and manage projects effectively.
+           We create powerful digital experiences through custom websites and mobile applications tailored to your business needs. 
+           From concept to deployment, we focus on innovation, performance, and user experience to help brands stand out and succeed.
           </motion.p>
         </div>
 
