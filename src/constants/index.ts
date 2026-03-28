dc350cf776c0076d62b49836393b6f9c07929cb6
Nithin2129 <vasishta.n1234@gmail.com>
2026-03-28 17:52:13 +0530

    Change Instagram link for Pruthvig
    Updated Instagram link for user Pruthvig.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -286,7 +286,7 @@ export const TEAM_MEMBERS = [
     ],
     image: "",
     github_link: "https://github.com/pruthvig",
-    instagram_link: "https://instagram.com/pruthvig",
+    instagram_link: "https://www.instagram.com/pruthvee_0106?igsh=MXV6d29heWJvejc4ag==",
     linkedin_link: "https://linkedin.com/in/pruthvig"
   },
   {
