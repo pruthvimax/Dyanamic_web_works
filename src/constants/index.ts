c8b7d7b49f0918bd64dd02b3f2eb963fddbb84bc
Nithin2129 <vasishta.n1234@gmail.com>
2026-03-28 19:44:26 +0530

    Changed harish profile pic
    Updated the image property to use Nithinpic.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -285,7 +285,7 @@ export const TEAM_MEMBERS = [
       { name: "JavaScript", color: "pink-text-gradient" },
       { name: "MongoDB", color: "blue-text-gradient" }
     ],
-    image: "",
+    image: Nithinpic,
     github_link: "https://github.com/pruthvimax",
     instagram_link: "https://www.instagram.com/pruthvee_0106?igsh=MXV6d29heWJvejc4ag==",
     linkedin_link: "https://linkedin.com/in/pruthvig"
